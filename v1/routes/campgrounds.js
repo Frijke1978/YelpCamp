1bb3f230ae40e41394fbdf2fcc23543c86946fcd
FrijkeUbuntu <FrijkeUbuntu@hotmail.com>
2020-01-22 12:07:41 +0100

    Adding EDIT option

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -59,6 +59,19 @@ router.get("/:id", function(req, res) {
     });
 });
 
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect("/campgrounds")
+        } else {
+            res.render("campgrounds/edit", { campground: foundCampground });
+        }
+    });
+});
+
+// UPDATE CAMPGROUND ROUTE
+
 
 //=== === === ==
 // MIDDLEWARE
