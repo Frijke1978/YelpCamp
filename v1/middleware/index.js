ac93311084388bb5c807e937078ce81e02ef288a
FrijkeUbuntu <FrijkeUbuntu@hotmail.com>
2020-01-24 15:08:29 +0100

    refactored middleware

---
+++ v1/middleware/index.js
@@ -0,0 +1,53 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if (err) {
+                res.redirect("/back");
+            } else {
+                // does he own the campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        // The user is not logged in
+        res.redirect("back");
+    }
+}
+
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("/back");
+            } else {
+                // does he own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        // The user is not logged in
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
